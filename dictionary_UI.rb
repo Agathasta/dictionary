707886a4736c3c120d29ca9cd1f1155ff59c8c74
agathasta <kalidonia@hotmail.com>
2020-11-20 17:10:42 +0100

    Add _loader

--- dictionary_UI.rb
+++ dictionary_UI.rb
@@ -4,5 +4,21 @@ require_relative 'dictionary_searcher'
 require_relative 'results_saver'
 
 class DictionaryUI
-  
+
+  def initialize
+    @diccionary = DictionaryLoader.new(ask_path)
+  end
+
+  def ask_path
+    puts "Is your dictionary's path = '5desk.txt'? (Y / N)"
+    if gets.chomp == "Y"
+      '5desk.txt'
+    else
+      puts "So what's the path then?"
+      gets.chomp
+    end
+  end
 end
+
+d = DictionaryUI.new
+#d.run
--- dictionary_loader.rb
+++ dictionary_loader.rb
@@ -1,3 +1,12 @@
+
 class DictionaryLoader
-  
+
+  def initialize(path)
+    @path = path
+    load_diccionary
+  end
+
+  def load_diccionary
+    @diccionary = File.readlines(@path)
+  end
 end
