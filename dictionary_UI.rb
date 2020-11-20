0592abe15da24b9d631a2774b18444f85297b246
agathasta <kalidonia@hotmail.com>
2020-11-20 17:13:22 +0100

    Small style corrections

--- dictionary_UI.rb
+++ dictionary_UI.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
+
 require_relative 'dictionary_loader'
 require_relative 'dictionary'
 require_relative 'dictionary_searcher'
 require_relative 'results_saver'
 
 class DictionaryUI
-
   def initialize
     @diccionary = DictionaryLoader.new(ask_path)
   end
 
   def ask_path
     puts "Is your dictionary's path = '5desk.txt'? (Y / N)"
-    if gets.chomp == "Y"
+    if gets.chomp == 'Y'
       '5desk.txt'
     else
       puts "So what's the path then?"
@@ -21,4 +22,4 @@ class DictionaryUI
 end
 
 d = DictionaryUI.new
-#d.run
+# d.run
--- dictionary_loader.rb
+++ dictionary_loader.rb
@@ -1,6 +1,6 @@
+# frozen_string_literal: true
 
 class DictionaryLoader
-
   def initialize(path)
     @path = path
     load_diccionary
