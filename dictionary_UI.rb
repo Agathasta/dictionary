19f3ed4806a2de850a8a4971a1c4fdce65df2f65
agathasta <kalidonia@hotmail.com>
2020-11-23 13:43:24 +0100

    Add quit to _UI

--- dictionary_UI.rb
+++ dictionary_UI.rb
@@ -11,20 +11,24 @@ class DictionaryUI
   end
 
   def ask_path
-    puts "Is your dictionary called '5desk.txt'? (Y / N)"
-    if gets.chomp.upcase == 'Y'
+    puts "Is your dictionary called '5desk.txt'? (Y / N, q to quit)"
+    answer = gets.chomp
+    if answer.upcase == 'Y'
       '5desk.txt'
+    elsif answer.upcase == 'Q'
+      exit
     else
       other_path
     end
   end
 
   def other_path
-    puts "So what's the path then?"
+    puts "So what's the path then? (q to quit)"
     loop do
       @path = gets.chomp
+      exit if @path.upcase == 'Q'
       break if File.exist?(@path)
-      puts "File doesn't exist! Enter a new file:"
+      puts "File doesn't exist! Enter a new file: (q to quit)"
     end
     @path
   end
