21ad3da3f87b5370885ab89e1e51ca223718695f
agathasta <kalidonia@hotmail.com>
2020-11-24 16:01:03 +0100

    Finish _saver

--- dictionary_UI.rb
+++ dictionary_UI.rb
@@ -14,27 +14,36 @@ class DictionaryUI
   end
 
   def ask_path
+    puts
     puts "Is your dictionary called '5desk.txt'? (Y / N)"
+    print '> '
     gets.chomp.upcase == 'Y' ? '5desk.txt' : other_path
   end
 
   def other_path
     puts "So what's the path then? (q to quit)"
+    print '> '
     loop do
       @path = gets.chomp
       exit if @path.upcase == 'Q'
       break if File.exist?(@path)
 
       puts "File doesn't exist! Enter a new file: (q to quit)"
+      print '> '
     end
     @path
   end
 
   def stats
-    puts 'Dictionary successfully loaded' unless @dictionary.display.nil?
-    puts "Your dictionary contains #{@dictionary.display.size} words."
-    puts 'Word frequency by starting letter:'
-    @dictionary.count_words
+    puts 'Dictionary successfully loaded' unless @dictionary.display.empty?
+    puts 'Show statistics? (Y / N)'
+    print '> '
+    if gets.chomp.upcase == 'Y'
+      puts 'Word frequency by starting letter:'
+      @dictionary.count_words
+      puts "Your dictionary contains #{@dictionary.display.size} words."
+      puts
+    end
   end
 
   def search
@@ -49,27 +58,57 @@ class DictionaryUI
     puts '# 2: Partial'
     puts '# 3: Begins With'
     puts '# 4: Ends With'
+    print '> '
     loop do
       @choice = gets.chomp
       exit if @choice.upcase == 'Q'
       break if (1..4).include? @choice.to_i
 
       puts "Wrong choice! Choose a number from 1 to 4 (or q to quit):"
+      print '> '
     end
   end
 
   def search_term
     puts 'Enter the search term:'
-    search_term = gets.chomp.downcase
-    @search = DictionarySearcher.new(@dictionary.display, @choice.to_i, search_term)
+    print '> '
+    @search_term = gets.chomp.downcase
+    @search = DictionarySearcher.new(@dictionary.display, @choice.to_i, @search_term)
   end
 
   def results
+    puts 'Results:'
     puts @search.search
     puts "#{@search.search.size} words found"
+  end
+
+  def save
+    puts "Do you want to save the results? (Y / N)"
+    print '> '
+    exit if gets.chomp.upcase == "N"
+    filepath
+    save_file
+  end
+
+  def filepath
+    puts 'What filepath should we write results to? (.txt)'
+    print '> '
+    @path = gets.chomp
+    if File.exist?(@path)
+      puts 'That file exists, overwrite? (Y / N)'
+      print '> '
+      exit if gets.chomp.upcase == "N"
+    end
+  end
+
+  def save_file
+    @saved_results = ResultsSaver.new(@path, @search_term, @search.search)
+    @saved_results.write
+    puts 'File successfully overwritten!'
   end
 end
 
 d = DictionaryUI.new
-# d.stats
+d.stats
 d.search
+d.save
--- results_saver.rb
+++ results_saver.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class ResultsSaver
+  def initialize(path, search_term, search)
+    @search_term = search_term
+    @search = search
+    @path = path
+  end
+
+  def write
+    File.open(@path,'w') do |file|
+      file.write "You searched for #{@search_term}\n"
+      file.write @search
+    end
+  end
 end
